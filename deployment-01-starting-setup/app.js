34f7034562b97ffbbfeac7644c89482c2d934786
karribalu <karri.balu123456@gmail.com>
2025-04-03 21:52:10 +0100

    Kubernetes deployment and service

--- deployment-01-starting-setup/app.js
+++ deployment-01-starting-setup/app.js
@@ -1,14 +1,19 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  const filePath = path.join(__dirname, 'views', 'welcome.html');
+app.get("/", (req, res) => {
+  const filePath = path.join(__dirname, "views", "welcome.html");
   res.sendFile(filePath);
+});
+
+app.get("/error", (req, res) => {
+  // should stop the entire server
+  process.exit(1);
 });
 
 app.listen(80);
